999f09208211642eadb0bc2106b6eb151d9a304d
mbalcerzak <malgorzata.a.balcerzak@gmail.com>
2021-06-13 23:32:27 +0200

    price changes history, fixed axis, fixed Wilanow

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_daq as daq
 import pandas as pd
 import plotly.express as px
 from dash.dependencies import Output, Input
@@ -32,6 +33,18 @@ def today_str():
 
 
 today = today_str()
+
+
+def get_dates():
+    data = get_json()
+    dff = data["dates"]
+    min_date = dff['min_date']
+    max_date = dff['max_date']
+
+    return min_date, max_date
+
+
+date_first, date_last = get_dates()
 
 districts = get_list("flats_per_location")
 flat_sizes = get_list("flats_per_area_cat")
@@ -70,7 +83,7 @@ app.layout = html.Div([
                    'justifyContent': 'center'},
         ),
         html.Div([
-            html.H1('Overall statistics',
+            html.H1(f'From {date_first} to {date_last}',
                     style={'textAlign': 'center',
                            'color': '#1f3b4d',
                            'fontSize': '30px',
@@ -94,6 +107,10 @@ app.layout = html.Div([
                           style={'padding': '25px'}),
             ),
             html.Div(
+                dcc.Graph(id='price-changes-per-day',
+                          style={'padding': '25px'}),
+            ),
+            html.Div(
                 dcc.Graph(id='all-districts-prices',
                           style={'padding': '25px'}),
             ),
@@ -106,39 +123,53 @@ app.layout = html.Div([
                            'padding-top': '15px'},
                     ),
             html.Div([
-                html.Label('Select district'),
-                dcc.Dropdown(
-                    id='district-dropdown',
-                    options=districts,
-                    value='Mokotów',
-                    multi=False,
-                    clearable=True,
-                    searchable=True,
-                    placeholder='Choose a City...',
+                html.Div([
+                    html.Label('Select district'),
+                    dcc.Dropdown(
+                        id='district-dropdown',
+                        options=districts,
+                        value='Mokotów',
+                        multi=False,
+                        clearable=True,
+                        searchable=True,
+                        placeholder='Choose a district...',
+                    ),
+                ], className="five columns",
+                    style={'width': '25%',
+                           'display': 'inline-block',
+                           'padding-left': '150px',
+                           'padding-top': '20px'}
                 ),
-            ],
-                style={'width': '25%',
-                       'display': 'inline-block',
-                       'padding-left': '150px',
-                       'padding-top': '20px'}
-            ),
-            html.Div([
-                html.Label('Select a flat size (square metres)'),
-                dcc.Dropdown(
-                    id='area-dropdown',
-                    options=flat_sizes,
-                    value='40_50',
-                    multi=False,
-                    clearable=True,
-                    searchable=True,
-                    placeholder='Choose a City...',
+                html.Div([
+                    html.Label('Select a flat size (square metres)'),
+                    dcc.Dropdown(
+                        id='area-dropdown',
+                        options=flat_sizes,
+                        value='40_50',
+                        multi=False,
+                        clearable=True,
+                        searchable=True,
+                        placeholder='Choose a flat size...',
+                    ),
+                ], className="five columns",
+                    style={'width': '25%',
+                           'display': 'inline-block',
+                           'padding-left': '50px',
+                           'padding-top': '20px'}
                 ),
-            ],
-                style={'width': '25%',
-                       'display': 'inline-block',
-                       'padding-left': '150px',
-                       'padding-top': '20px'}
-            ),
+                html.Div([
+                    html.Label('Fix price axis (easier to compare)'),
+                    daq.ToggleSwitch(
+                        id='price-axis-toggle',
+                        value=False
+                    ),
+                    html.Div(id='toggle-switch-output')
+                ], className="one column",
+                    style={'width': '25%',
+                           'display': 'inline-block',
+                           'padding-left': '30px',
+                           'padding-top': '30px'}),
+            ], className="row"),
             html.Div(
                 dcc.Graph(id='district-area-price',
                           style={'padding': '25px'}),
@@ -167,18 +198,27 @@ app.layout = html.Div([
 @app.callback(
     Output('district-area-price', 'figure'),
     [Input('district-dropdown', "value"),
-     Input('area-dropdown', "value")])
-def update_figure(location, area):
+     Input('area-dropdown', "value"),
+     Input('price-axis-toggle', "value")])
+def update_figure(location, area, toggle):
     data = get_json()
 
     dff = data["price_m_loc_area_cat"]
     dff = pd.DataFrame.from_dict(dff)
+    prices = [x for x in dff['avg_price_per_m'].to_list() if x < 50000]
+
+    max_price = max(prices)
+    min_price = min(prices)
+
     dff = dff[dff['location'] == location]
     dff = dff[dff['area_category'] == area]
     num_flats = sum(dff['num_flats'])
 
     dff = dff.sort_values(by=['month_num'])
     fig = px.line(dff, x='month', y='avg_price_per_m')
+
+    if toggle:
+        fig.update_yaxes(range=[min_price, max_price])
 
     fig.update_layout(template='xgridoff',
                       yaxis={'title': 'Price per m2 (PLN)'},
@@ -207,7 +247,7 @@ def update_figure(area):
     fig.update_layout(template='xgridoff',
                       yaxis={'title': 'Average price per m2 (PLN)'},
                       xaxis={'title': 'Month'},
-                      title={'text': f'Average prices per m2 for each district',
+                      title={'text': f'Average prices per m2 for each district (flats of all sizes)',
                              'font': {'size': 24}, 'x': 0.5, 'xanchor': 'center'}
                       )
 
@@ -233,15 +273,43 @@ def update_figure(area):
     df = df.sort_values(by=['Date'])
 
     df_full = df.loc[df['Date'] != today]
-    date_first = min(df_full['Date'])
-    date_last = max(df_full['Date'])
 
     fig = px.line(df_full, x='Date', y='Value', color='Type')
 
     fig.update_layout(template='xgridoff',
                       yaxis={'title': 'Number of ads scraped'},
                       xaxis={'title': 'Date'},
-                      title={'text': f'Ads scraped between {date_first} and {date_last}',
+                      title={'text': f'Ads scraped daily',
+                             'font': {'size': 24}, 'x': 0.5, 'xanchor': 'center'}
+                      )
+    return fig
+
+
+@app.callback(
+    Output('price-changes-per-day', 'figure'),
+    Input('area-dropdown', 'value'))
+def update_figure(area):
+    data = get_json()
+    dff = data["changes_per_day"]
+    dff = pd.DataFrame(dff.items(), columns=['Date', 'Value'])
+
+    dff['Type'] = 'Value'
+
+    dff_ma = data["changed_per_day_m_avg"]
+    dff_ma = pd.DataFrame(dff_ma.items(), columns=['Date', 'Value'])
+    dff_ma['Type'] = 'Moving Average (7 days)'
+
+    df = dff.append(dff_ma, ignore_index=True)
+    df = df.sort_values(by=['Date'])
+
+    df_full = df.loc[df['Date'] != today]
+
+    fig = px.line(df_full, x='Date', y='Value', color='Type')
+
+    fig.update_layout(template='xgridoff',
+                      yaxis={'title': 'Number of price changes per day'},
+                      xaxis={'title': 'Date'},
+                      title={'text': 'Daily price changes',
                              'font': {'size': 24}, 'x': 0.5, 'xanchor': 'center'}
                       )
     return fig
